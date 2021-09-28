b85116a0400d8b0be6f7a0fdef0899bb8ca7faa7
ashukla33317 <amanshukla33317@gmail.com>
2021-09-28 21:30:02 +0530

    navbar burger

---
+++ index.js
@@ -0,0 +1,20 @@
+const mainmenu = document.querySelector('.mainmenu');
+const closemenu = document.querySelector('.closemenu');
+const openmenu = document.querySelector('.openmenu');
+
+
+function show() {
+    mainmenu.style.display ='flex';
+    mainmenu.style.top ='0';
+
+    
+}
+
+function close() {
+    mainmenu.style.top ='-100%';
+
+    
+}
+
+openmenu.addEventListener('click',show);
+closemenu.addEventListener('click',close);
